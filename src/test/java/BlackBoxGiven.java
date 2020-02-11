31179192c1c1a5ad84415a2199c9f6055fb5a006
thamiti1 <33884208+thamiti1@users.noreply.github.com>
2020-02-10 21:20:42 -0700

    Added 2 more tests testing age and discount
    Discount is in relation to frozen food and alcohol

--- src/test/java/BlackBoxGiven.java
+++ src/test/java/BlackBoxGiven.java
@@ -52,7 +52,12 @@ public class BlackBoxGiven {
     Cart cart1;
     double cart1Expected;
 
+    Cart cart2;
+    double cart2Expected;
 
+    Cart cart3;
+    double cart3Expected;
+    
     @org.junit.Before
     public void setUp() throws Exception {
 
@@ -72,6 +77,37 @@ public class BlackBoxGiven {
 
         cart1Expected = 70.2;
         
+        
+        cart2 = createCart(10);
+        for(int i = 0; i < 3; i++) {
+            cart2.addItem(new Dairy());
+        }
+        for(int i = 0; i < 2; i++) {
+            cart2.addItem(new Produce());
+        }
+        for(int i = 0; i < 2; i++) {
+            cart2.addItem(new FrozenFood());
+        }    
+
+        cart2Expected = 35.64;
+        
+        
+        cart3 = createCart(100);
+        for (int i = 0; i < 2; i++) {
+            cart3.addItem(new Alcohol());
+       }
+        for(int i = 0; i < 3; i++) {
+            cart3.addItem(new Dairy());
+        }
+        for(int i = 0; i < 2; i++) {
+            cart3.addItem(new Produce());
+        }
+        for(int i = 0; i < 2; i++) {
+            cart3.addItem(new FrozenFood());
+        }    
+
+        cart3Expected = 35.64;
+        
     }
 
     // sample test
@@ -79,5 +115,21 @@ public class BlackBoxGiven {
     public void calcCostCart1() throws UnderAgeException {
         double amount = cart1.calcCost();
         assertEquals(cart1Expected, amount, .01);
+    }
+    
+ // Tests whether someone under age is allowed to buy products with no alcohol
+ // This fails case 2 even though the person who is 10 is not buying alcohol
+    @Test
+    public void calcCostCart2() throws UnderAgeException {
+        double amount = cart2.calcCost();
+        assertEquals(cart2Expected, amount, .01);
+    }
+    
+ // Tests whether or not the discount for alcohol and frozen food is applied
+ // Fails Case 3 value of 42.12 is not discounted  
+    @Test
+    public void calcCostCart3() throws UnderAgeException {
+        double amount = cart3.calcCost();
+        assertEquals(cart3Expected, amount, .01);
     }
 }
