c2f24bcb330145a0259b67b96f82c3209f29ac9d
thamiti1 <33884208+thamiti1@users.noreply.github.com>
2020-03-16 21:50:17 -0700

    Fixed Checkstyle errors under 25

--- src/main/java/Alcohol.java
+++ src/main/java/Alcohol.java
@@ -1,4 +1,4 @@
- package main.java;
+package main.java;
 
 public class Alcohol extends Product {
     public Alcohol() {
--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -11,8 +11,8 @@ public class Cart {
 
     /**
      * Calculates the final cost after all savings and tax has been applied. Also checks
-     * that the user is of age to purchase alcohol if it is in their cart at checkout. Sales tax is always AZ tax.
-     *
+     * that the user is of age to purchase alcohol if it is in their cart at checkout. 
+     * Sales tax is always AZ tax.
      * Calculation is based off of the following prices and deals:
      * Dairy -> $3
      * Meat -> $10
@@ -20,98 +20,101 @@ public class Cart {
      * Alcohol -> $8
      * Frozen Food -> $5
      * Alcohol + Frozen Food -> $10
-     *
      * If there is an alcohol product in the cart and the user is under 21, then an
      * UnderAgeException should be thrown.
      *
      * @return double totalCost
-     * @throws UnderAgeException
      */
     public double calcCost() throws UnderAgeException {
-    	double cost = 0.0;
-    	double tax = 0.0;
-    	double totalCost = 0.0;
-    	
-    	int a = 0; //alcohol
-    	int f = 0; //frozen food
-    	int p = 0; //produce
-    	
-    	for(int i = 0; i < cart.size(); i++) {
-    		if(cart.get(i).getClass() == Alcohol.class) {
-    			a++;
-    		}
-    		
-    		else if(cart.get(i).getClass() == FrozenFood.class) {
-    			f++;
-    		}
-    		
-    		if(a >= 1 && f >= 1) {
-    			cost -= 3;
-    			a--;
-    			f--;
-    		}
-    		
-    		if(cart.get(i).getClass() == Produce.class) {
-    			p++;
-    			
-    			if(p >= 3) {
-        			cost -=1;
-        			p = 0;
-        		}
-    		}
-    		
-    		cost += cart.get(i).getCost();    		
-    	}
-    	tax = getTax(cost, "AZ");
-    	totalCost = tax + cost;
-    	
+        double cost = 0.0;
+        double tax = 0.0;
+        double totalCost = 0.0;
+        
+        int a = 0; //alcohol
+        int f = 0; //frozen food
+        int p = 0; //produce
+        
+        for (int i = 0; i < cart.size(); i++) {
+            if (cart.get(i).getClass() == Alcohol.class) {
+                a++; 
+            
+            } else if (cart.get(i).getClass() == FrozenFood.class) {
+                f++;
+            }
+   
+            if (a >= 1 && f >= 1) {
+                cost -= 3;
+                a--;
+                f--;
+            }
+    
+            if (cart.get(i).getClass() == Produce.class) {
+                p++;
+    
+                if (p >= 3) {
+                    cost -= 1;
+                    p = 0;
+                }
+            }
+    
+            cost += cart.get(i).getCost();    
+        }
+        tax = getTax(cost, "AZ");
+        totalCost = tax + cost;
+    
         return totalCost; 
     }
 
-    // calculates how much was saved in the current shopping cart based on the deals, returns the saved amount
+    // calculates how much was saved in the current shopping cart based on the deals, 
+    // returns the saved amount
     // throws exception if alcohol is bought from underage person
-    // TODO: Create node graph for this method in assign 4: create white box tests and fix the method, reach at least 98% coverage
+    // TODO: Create node graph for this method in assign 4: create white box tests and 
+    // fix the method, reach at least 98% coverage
+    
     public int Amount_saved() throws UnderAgeException {
         int subTotal = 0;
         int costAfterSavings = 0;
 
-        double produce_counter = 0;
+        double produceCounter = 0;
         int alcoholCounter = 0;
         int frozenFoodCounter = 0;
         int dairyCounter = 0;
 
-        for(int i = 0; i < cart.size(); i++) {
+        for (int i = 0; i < cart.size(); i++) {
             subTotal += cart.get(i).getCost();
-            costAfterSavings =costAfterSavings+cart.get(i).getCost();
+            costAfterSavings = costAfterSavings + cart.get(i).getCost();
 
-            if (cart.get(i).getClass().toString().equals (Produce.class.toString())) {  //SER316
-                produce_counter++;				//.equals so it worked properly for coverage
+            if (cart.get(i).getClass().toString().equals(Produce.class.toString())) {  //SER316
+                produceCounter++;    //.equals so it worked properly for coverage
 
-                if (produce_counter >= 3) {
+                if (produceCounter >= 3) {
                     costAfterSavings -= 1;
-                    produce_counter = 0;
+                    produceCounter = 0;
                 } 
-            }
-            else if (cart.get(i).getClass().toString().equals (Alcohol.class.toString())) {  //SER316
-                alcoholCounter++;					 //.equals so it worked properly for coverage
+            
+            } else if (cart.get(i).getClass().toString().equals(Alcohol.class.toString())) {  
+                //SER316
+                alcoholCounter++;        //.equals so it worked properly for coverage
                 if (userAge < 21) {
                     throw new UnderAgeException("The User is not of age to purchase alcohol!");
                 }
+            } else if (cart.get(i).getClass().toString().equals(FrozenFood.class.toString())) { 
+                //SER 316
+                frozenFoodCounter++;    //.equals so it worked for code coverage
             }
-            
-            else if (cart.get(i).getClass().toString().equals (FrozenFood.class.toString())) { //SER 316
-                frozenFoodCounter++;				//.equals so it worked for code coverage
-            }
-			/**SER 316 start
-			*	else if (cart.get(i).getClass().toString() == FrozenFood.class.toString())
+            /**SER 316 start
+             * else if (cart.get(i).getClass().toString() == FrozenFood.class.toString())
             *    dairyCounter++;
-			* SER 316 end 
-			*/   //code was redundant and dairy is incorrectly incremented, doesn't need to in this case
-			
+            * SER 316 end 
+            * code was redundant and dairy is incorrectly incremented, 
+            * doesn't need to in this case
+            */ 
+
+
             if (alcoholCounter >= 1 && frozenFoodCounter >= 1) {
-                 costAfterSavings = costAfterSavings + 3;
-                 alcoholCounter--;
-                 frozenFoodCounter--;
+                costAfterSavings = costAfterSavings + 3;
+                alcoholCounter--;
+                frozenFoodCounter--;
             }
         }
 
@@ -140,17 +143,16 @@ public class Cart {
     }
 
     public void addItem(Product np) {
-      cart.add(np);
+        cart.add(np);
     }
 
-    public boolean RemoveItem(Product productToRemove)
-    {
-    		boolean test = false;
+    public boolean RemoveItem(Product productToRemove) {
+        boolean test = false;
         for (int i = 0; i < cart.size(); i++) {
             if (cart.get(i) == productToRemove) {
-                 cart.remove(i);
-                 test = true;
-                 return test;
+                cart.remove(i);
+                test = true;
+                return test;
             }
         }
         return false;
